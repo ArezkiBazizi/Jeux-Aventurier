2df578005612523662e84f5084e8b9c32aac8328
AREZKI BAZIZI <baziziarezki@gmail.com>
2024-01-11 22:11:43 +0100

    okpush

--- JeuxPOO/Aventurier.cpp
+++ JeuxPOO/Aventurier.cpp
@@ -75,29 +75,29 @@ void Aventurier::deplacerA( Terrain& T) {
             
         }
 
-        
+
 
         // Appliquer la logique de déplacement en fonction de l'état des touches
         if (touchesDirection[0])
         {
-           
-           
-            if (T.retourneC(position().x(), position().y() + 1).type() == "Vide" ) {
-                
-  
-                deplacer(0, 1);// Up
-                
+
+
+            if (T.retourneC(position().x(), position().y() + 1).type() == "Vide") {
+
+
+
+                switchCases(T.retourneC(position().x(), position().y() + 1));
 
 
             }
         }
         if (touchesDirection[1])
         {
-        if (T.retourneC(position().x(), position().y() - 1).type() == "Vide") {
-            
-          
-                deplacer(0, -1);  // Down
-                
+            if (T.retourneC(position().x(), position().y() - 1).type() == "Vide") {
+
+
+                switchCases(T.retourneC(position().x(), position().y() - 1));
+
 
 
             }
@@ -105,10 +105,10 @@ void Aventurier::deplacerA( Terrain& T) {
         if (touchesDirection[2])
         {
             if (T.retourneC(position().x() - 1, position().y()).type() == "Vide") {
-            
-    
-                deplacer(-1, 0); // Left
-                
+
+
+                switchCases(T.retourneC(position().x() - 1, position().y()));
+
 
 
             }
@@ -117,8 +117,7 @@ void Aventurier::deplacerA( Terrain& T) {
         {
             if (T.retourneC(position().x() + 1, position().y()).type() == "Vide") {
 
-                deplacer(1, 0);  // Right
-               
+                switchCases(T.retourneC(position().x() + 1, position().y()));
 
             }
         }
