5193445ed8d231e6bd57294422f90050b3af6b61
AREZKI BAZIZI <baziziarezki@gmail.com>
2024-01-11 23:37:50 +0100

    push nouveau terrain fonctionnel

--- JeuxPOO/Game.cpp
+++ JeuxPOO/Game.cpp
@@ -5,15 +5,14 @@ Game::Game(const Position& posAventurier)
 {
 
 	Terrain Ter{ 1000,1000 };
-	//Ter.litTerrain();
-	//Ter.retourne();
+	Ter.litTerrain();
+
 	Aventurier* a = Ter.trouverAventurier();
 
 	Ter.afficheTerrain();
 	while (a->estVivant()) {
 		
 		a->deplacerA(Ter);
-		cout << "/";
 	}
 		
 }
