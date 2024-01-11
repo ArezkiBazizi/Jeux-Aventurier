68c8c7d695891c04a28f36f7dfda4b65d57fb902
AREZKI BAZIZI <baziziarezki@gmail.com>
2024-01-11 15:07:27 +0100

    push

--- JeuxPOO/Game.cpp
+++ JeuxPOO/Game.cpp
@@ -1,20 +1,14 @@
 #include "game.h"
 
 
-class Game{
-
-	Game::Game(const Position& posAventurier)
+Game::Game(const Position& posAventurier)
 	{
 
 		Terrain Ter{ 1000,1000 };
-		make_unique<Aventurier>(a{Position{1,1} });
+		unique_ptr<Aventurier> a = make_unique<Aventurier>( Position{1,1} );
+
+
 		Ter.AjoutPersonnage(a);
-	}
-
-
-	Terrain Ter{ 1000,1000 };
-	Aventurier a{ Position{1,1} };
-	MonstreV m{ Position{1,1} };
-	Ter.AjoutePersonnage();
-
 }
+
+
